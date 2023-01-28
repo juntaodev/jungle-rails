b556df66933379807d8c1e27bd3f28c970436ec8
juntaodev <juntaoliudev@gmail.com>
2023-01-28 00:38:33 -0500

    fix bug: add admin securiry

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: Rails.configuration.admin[:user], password: Rails.configuration.admin[:password]
   def show
   end
 end
