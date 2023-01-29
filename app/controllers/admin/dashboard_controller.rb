af4590d326506b911f6a9cbe8b26d429a167cc02
juntaodev <juntaoliudev@gmail.com>
2023-01-28 22:19:53 -0500

    fix feature: admin dashboard count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: Rails.configuration.admin[:user], password: Rails.configuration.admin[:password]
   def show
+    @products = Product.all
+    @category = Category.all
   end
 end
