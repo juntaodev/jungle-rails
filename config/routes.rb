99b99ed5acb4f1b8f12552e951f6b701baa18521
juntaodev <juntaoliudev@gmail.com>
2023-01-28 23:01:54 -0500

    fix: add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-
+  get'/about' => 'about#index'
+  
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
